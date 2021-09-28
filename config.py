dbb4a8d4f157ef6b9024b99eb273e9a5727af86c
Brady Dorsey <61926486+fusion407@users.noreply.github.com>
2021-09-28 13:44:11 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,35 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+TILESIZE = 32
+FPS = 60
+
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+ENEMY_SPEED = 1
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B.....B............B',
+    'B..................B',
+    'B..................BBBBBBBBBBB',
+    'BBBBBBB............B.........B',
+    'B..................B.........B',
+    'B......................BBB...',
+    'B.........P............BBB...',
+    'B..................B.........B',
+    'B....E.............B.........B',
+    'B..................BBBBBBBBBBB',
+    'B..................B',
+    'B.............E....B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
---
+++ main.py
@@ -0,0 +1,104 @@
+import pygame, sys
+from sprites import *
+from config import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.font = pygame.font.Font('OldSchoolAdventures-42j9.ttf', 32)
+
+        # importing sprite sheets
+        self.character_spritesheet = Spritesheet('img/character.png')
+        self.terrain_spritesheet = Spritesheet('img/terrain.png')
+        self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.intro_background = pygame.image.load('./img/9.png')
+    
+    def createTilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                Ground(self, j, i)
+                if column == "B":
+                    Block(self, j, i)
+                if column == "E":
+                    Enemy(self, j, i)
+                if column == "P":
+                    Player(self, j, i)
+                    
+    def new(self):
+        # a new game starts
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.createTilemap()
+
+    def events(self):
+        # game loop events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        # game loop updates
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+    
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+    
+    def intro_screen(self):
+        intro = True
+
+        title = self.font.render('RPG Project', True, WHITE)
+        title_rect = title.get_rect(x=180, y=10)
+
+        play_button = Button(250, 80, 100, 50, WHITE, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+            
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
