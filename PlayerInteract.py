9678a28bc075c70f09a20675f80e69caeb0ea590
Brady Dorsey <61926486+fusion407@users.noreply.github.com>
2021-09-30 16:23:01 -0500

    Add files via upload

---
+++ PlayerInteract.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self):
+        interactable = False
+    def add_to_inventory(self):
+        pass
+    def remove_from_inventory(self):
+        pass
+    def use(self):
+        pass
